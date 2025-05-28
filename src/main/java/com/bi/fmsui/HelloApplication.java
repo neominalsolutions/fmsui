c9e09305294b24045b1d0b4dbf7028fdb0691c6e
neominal.solutions <neominal.solutions@gmail.com>
2025-05-28 16:01:58 +0300

    Son güncelleme

--- src/main/java/com/bi/fmsui/HelloApplication.java
+++ src/main/java/com/bi/fmsui/HelloApplication.java
@@ -7,14 +7,24 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
+
+
 public class HelloApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("freight-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Freight App!");
         stage.setScene(scene);
         stage.show();
+
+
+
+
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.bi.fmsui {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires transitive com.freight;
+
 
     opens com.bi.fmsui to javafx.fxml;
     exports com.bi.fmsui;
