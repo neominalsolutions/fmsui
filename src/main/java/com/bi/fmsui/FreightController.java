2c86bbbc3f8850c10f0650d640e8180cc96d7ce4
neominal.solutions <neominal.solutions@gmail.com>
2025-05-28 12:10:47 +0300

    Son güncelleme

--- src/main/java/com/bi/fmsui/FreightController.java
+++ src/main/java/com/bi/fmsui/FreightController.java
@@ -1,6 +1,9 @@
 package com.bi.fmsui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
 public class FreightController {
@@ -14,5 +17,15 @@ public class FreightController {
     private TextField txtLength;
     @FXML
     private TextField txtUnitPrice;
+    @FXML
+    private ComboBox<String> cmbFreightType;
+    @FXML
+    private RadioButton rbLight; // Hafif Paket
+    @FXML
+    private RadioButton rbHeavy; // Ağır Paket
+    @FXML
+    private DatePicker dtShippedDate; // Gönderim tarihi
+
+
 
 }
